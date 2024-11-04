312e4febe266b1455e269d70ea81e21758cbf153
Weronika Stasiak <82800@student.ue.poznan.pl>
2024-11-04 17:36:42 +0100

    adjusted input

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 def extract_article_data(article_url):
     response = requests.get(article_url)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.content, "html.parser")
 
-    # Ekstrakcja nazw odnośników do innych artykułów (wewnętrznych) i ich treści
     links = soup.select('a[href^="/wiki/"]')
     article_links = []
 
@@ -19,20 +17,16 @@ def extract_article_data(article_url):
         if len(article_links) >= 5:
             break
 
-    # Ekstrakcja URL-i obrazków
     images = soup.select('img')
     image_urls = ["https:" + img['src'] for img in images if 'src' in img.attrs][:3]
 
-    # Ekstrakcja URL-i źródeł zewnętrznych
     external_links = soup.select('a.external')
     external_urls = [link['href'] for link in external_links if 'href' in link.attrs][:3]
 
-    # Ekstrakcja nazw kategorii przypisanych do artykułu
     categories = soup.select('#mw-normal-catlinks ul li a')
     category_names = [category.get_text(strip=True) for category in categories][:3]
 
-    # Dodanie "(ujednoznacznienie)" dla odpowiednich nazw
-    formatted_links = [f"{link} (ujednoznacznienie)" if "ujednoznacznienie" in link.lower() else link for link in
+    formatted_links = [f"{link} (ujednoznaczniczenie)" if "ujednoznacznienie" in link.lower() else link for link in
                        article_links]
 
     return {
@@ -42,11 +36,7 @@ def extract_article_data(article_url):
         "categories": category_names or [""]
     }
 
-
-def main():
-    # Pobranie nazwy kategorii od użytkownika
-    category_name = input("Podaj nazwę kategorii na Wikipedii: ")
-
+def main(category_name):
     category_url = f"https://pl.wikipedia.org/wiki/Kategoria:{category_name.replace(' ', '_')}"
     response = requests.get(category_url)
     response.encoding = 'utf-8'
@@ -57,7 +47,6 @@ def main():
 
     soup = BeautifulSoup(response.content, "html.parser")
 
-    # Pobieranie linków do pierwszych dwóch artykułów z kategorii
     article_links = ["https://pl.wikipedia.org" + a['href'] for a in soup.select('.mw-category a')][:2]
 
     results = []
@@ -65,7 +54,6 @@ def main():
     for article_url in article_links:
         data = extract_article_data(article_url)
 
-        # Formatowanie wyników dla każdego artykułu z zachowaniem kolejności i formatu
         formatted_data = (
             f"{' | '.join(data['links'])}\n"
             f"{' | '.join(data['images'])}\n"
@@ -74,8 +62,11 @@ def main():
         )
         results.append(formatted_data)
 
-    # Wyświetlenie wyników w odpowiednim formacie dla testów
     print("\n".join(results))
 
 if __name__ == "__main__":
-    main()
+    import sys
+
+    # Getting input directly from the test cases
+    category_name = sys.argv[1]  # The input will be provided by the autograding test
+    main(category_name)
