d7abec77ae142dda6885e062839ac1b177cbb981
Weronika Stasiak <82800@student.ue.poznan.pl>
2024-11-06 21:10:35 +0100

    new method for images and categories

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import urllib.parse
 
 def extract_article_data(article_url):
     response = requests.get(article_url)
@@ -7,30 +8,40 @@ def extract_article_data(article_url):
     html_content = response.text
 
     # Wyrażenie regularne dla wewnętrznych linków (linki zaczynające się od "/wiki/" bez ujednoznacznienia)
-    internal_links = re.findall(r'href="/wiki/([^":#]+?)"', html_content)
-    article_links = [link.replace('_', ' ') for link in internal_links[:5]]  # Pierwsze 5 linków wewnętrznych
+    article_links = []
+    excluded_namespaces = (
+        "Kategoria:", "Plik:", "Wikipedia:", "Pomoc:", "Portal:",
+        "Wikipedysta:", "MediaWiki:", "Szablon:", "Moduł:",
+        "Specjalna:", "Media:"
+    )
+
+    # Wyrażenie regularne do szukania linków wewnętrznych "/wiki/..." w artykule
+    for link in re.findall(r'<a href="/wiki/([^":#]+)"', html_content):
+        # Filtracja przestrzeni nazw poprzez sprawdzenie, czy link nie zawiera prefiksu z listy wykluczeń
+        if not any(link.startswith(namespace) for namespace in excluded_namespaces):
+            # Dekodowanie URL i zamiana "_" na spacje
+            title = urllib.parse.unquote(link).replace('_', ' ')
+            article_links.append(title)
+            # Przerwanie, gdy mamy 5 linków
+            if len(article_links) >= 5:
+                break
 
     # Wyrażenie regularne dla obrazów
     image_urls = re.findall(r'src="(//upload\.wikimedia\.org/[^"]+)"', html_content)[:3]
 
     # Wyrażenie regularne dla linków zewnętrznych
-    external_urls = re.findall(r'href="(https?://[^"]+)" class="external"', html_content)[:3]
+    external_urls = re.findall(r'class="reference-text">.*?<a rel="nofollow" class="external text" href="(https?://[^"]+)"', html_content, re.DOTALL)[:3]
 
-    # Wyrażenie regularne dla kategorii (zakładam, że są w divie 'mw-normal-catlinks')
+    # Wyrażenie regularne dla kategorii
     categories = re.findall(r'<div id="mw-normal-catlinks".*?>(.*?)</div>', html_content, re.DOTALL)
     category_links = re.findall(r'title="Kategoria:[^"]+">([^<]+)', categories[0]) if categories else []
-
-    # Formatowanie linków wewnętrznych - dodajemy "(ujednoznacznienie)", jeśli potrzeba
-    formatted_links = [
-        f"{link} (ujednoznacznienie)" if "ujednoznacznienie" in link.lower() else link for link in article_links
-    ]
-    formatted_categories = [category.strip() for category in category_links]
+    category_links = category_links[:3]
 
     return {
-        "links": formatted_links or [""],
+        "links": article_links or [""],
         "images": image_urls or [""],
         "external_urls": external_urls or [""],
-        "categories": formatted_categories or [""]
+        "categories": category_links or [""]
     }
 
 def main():
@@ -41,7 +52,7 @@ def main():
     html_content = response.text
 
     # Wyszukiwanie linków do pierwszych dwóch artykułów w kategorii
-    article_urls = re.findall(r'href="(/wiki/[^":#]+?)"', html_content)[:2]
+    article_urls = re.findall(r'<li><a href="(/wiki/[^":#]+?)"', html_content)[:2]
     full_article_urls = ["https://pl.wikipedia.org" + url for url in article_urls]
 
     results = []
